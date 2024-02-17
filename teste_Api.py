48a478adfe61038390d97d40f8d9b334d5fc06e5
David Palricas <davipalricas22@gmail.com>
2024-02-17 20:05:55 +0000

    Novo parâmetro de pesquisa e melhorias

--- teste_Api.py
+++ teste_Api.py
@@ -8,17 +8,28 @@ RANDOM = True #Variável que indica se a dá receitas aleatórias ou não
                   #Considerei verdadeira para que o utilizador não receba sempre as mesmas receitas
 MAX_CALORIES = 1286 #Número máximo de calorias que econtrei por engenharia reversa
 
+MAX_OFFSET = 900 #Número máximo de skips que a API permite
+
+
+def Show(image,meal_type,Ingredients,description,equipments):
+    print(f"Imagem da receita: {image}\n")
+    print(f"Tipo de refeição: {meal_type}\n")
+    print(f"Ingredientes necessários para a receita: {Ingredients}\n")
+    print(f"Todos os passos da receita : {description}\n")
+    print(f"Todos os equipamentos necessário para a receita : {equipments}\n")
 
 
 
-
-def Recipe_instructions(recipe):
-    equipaments = []
+def Get_and_Show_Recipe_instructions(recipe):
+    description = {} #Dicionário com o número do passo e a descrição do passo
+    all_equipaments = set() #Conjunto com todos os equipamentos necessários para a receita
+   
+    equipaments = [] #Lista com os equipamentos necessários para cada passo da receita
     oven = False
-    ingredients = []
+    ingredients = [] #Lista com os ingredientes necessários para cada passo da receita
     recipe_id = recipe[1]
-    query_recipe_info = f"https://api.spoonacular.com/recipes/{recipe_id}/analyzedInstructions?apiKey={API_KEY}"
-    response_info = requests.get(query_recipe_info) #Informações da receita selecionada
+    query = f"https://api.spoonacular.com/recipes/{recipe_id}/analyzedInstructions?apiKey={API_KEY}"
+    response_info = requests.get(query) #Informações da receita selecionada
                                                     #Contém os passos , o número de cada passo, ingredientes e equipamentos necessários
                   
     
@@ -30,16 +41,21 @@ def Recipe_instructions(recipe):
     for steps in response_info.json():
         for step in steps["steps"]:
             print(f"\tPasso nº{step['number']} : {step['step']}")
+
+            description[f"Passo nº {step['number']}"] = step["step"] #Dicionário com o número do passo e a descrição do passo
+
             if step["equipment"] == []:
                 print("Não existe informação sobre equipamentos necessários")
             else:
          
                for equipament in step["equipment"]:
-                   equipaments.append(equipament["name"])
+                   equipaments.append(equipament["name"]) #Adiciona o equipamento à lista de equipamentos necessários para o passo
+                   all_equipaments.add(equipament["name"]) #Adiciona o equipamento ao conjunto de  todos equipamentos  necessários para a receita
                    if equipament["name"] == "oven":
                        oven = True
     
                print(f"Equipamentos: {equipaments}")
+               equipaments = [] #Reinicia a lista de equipamentos para o próximo passo da receita
          
                if "temparture" in step and oven == True:  #Notei nos exemplos da documentação que o forno pode ter especificações próprias
                    print("Configurações do forno")  
@@ -55,9 +71,37 @@ def Recipe_instructions(recipe):
                 for ingredient in step["ingredients"]:
                     ingredients.append(ingredient["name"])
                 print(f"Ingredientes: {ingredients}")
+                ingredients = [] #Reinicia a lista de ingredientes para o proíxmo passso da receita
+
 
             print("\n")
+
+    return description,all_equipaments
+        
     
+def Get_Recipe_information(recipe_id):
+    Ingredients_info= {} #Dicionário com os ingredientes e as quantidades necessárias para a receita
+    query = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={API_KEY}"
+    response = requests.get(query) #Informações da receita selecionada
+
+    if response.json() == []: #Se a receita não tiver informação sobre os ingredientes necessários
+        print("Informação da receita indisponível")
+        print("Programa terminado")
+        exit(1)
+    
+    image = response.json()["image"] # Url da imagem da receita
+
+    meal_type = response.json()["dishTypes"] #Tipo de refeição
+
+
+    for ingredient in response.json()["extendedIngredients"]:
+        Ingredients_info[ingredient["name"]] = f"Quantidade {ingredient['measures']['metric']['amount']} {ingredient['measures']['metric']['unitShort']}"
+
+
+
+
+    return image,meal_type,Ingredients_info
+        
 
 
 def Ask_for_recipe(tag,recipes):
@@ -89,7 +133,10 @@ def Ask_for_recipe(tag,recipes):
 def Number_of_recipes():
     while True:
         try:
+            print("\033[91m\tAVISO!\033[0m")
+            print("1. O número de receitas máximo e receitas é 100 e o número mínimo é 1")
             print("Digite o número de receitas que deseja ver: ")
+         
             number_recipes = int(input())
             if number_recipes < 1 or number_recipes > 100:
                 print("Número inválido, tente novamente")
@@ -100,20 +147,41 @@ def Number_of_recipes():
     
     return number_recipes
 
+def Meal_type():
+    meal_type = ["main course","side dish","dessert","appetizer","salad","bread","breakfast","soup","beverage","sauce","marinade","fingerfood","snack","drink"]
 
+    print("\033[91m\tAVISOS!\033[0m")
+    print("1. O tipo de refeições têm de ser escritos em inglês (Fase de teste)")
+    print("2. Os tipos de refeições suportados")
+    for meal in meal_type:
+        print(f"\t.{meal}")
+
+    print("Digite o tipo de refeição que deseja pesquisar: ")
+
+    while True:
+        try:
+            meal = input()
+            if meal.lower() not in meal_type:
+                print("Tipo de refeição inválido, tente novamente")
+            else:
+                break
+        except ValueError:
+            print("Dado inválido, tente novamente")
+            print("Digite o tipo de refeição que deseja pesquisar: ")
+
+    number_recipes = Number_of_recipes()
+
+        #Como neste tipo de pesquisa não existe o parãmetro random, decidi adicionar um offset aleatório
+        #para que o utilizador não receba sempre as mesmas receitas
+    offset = random.randint(0,MAX_OFFSET) #Número aleatório entre 0 e 900
+    return f"&type={meal}&number={number_recipes}&offset={offset}" #query_params
+    
 
 def Random():
+    offset = random.randint(0,MAX_OFFSET) #Número aleatório entre 0 e 900
     number_recipes = Number_of_recipes()
-    min_calories = random.randint(0,MAX_CALORIES) 
 
-    if min_calories == MAX_CALORIES-1:
-        max_calories = MAX_CALORIES
-    else:
-        max_calories = random.randint(min_calories+1,MAX_CALORIES+1) 
-
-
-
-    return f"&minCalories={min_calories}&maxCalories={max_calories}&number={number_recipes}&random={RANDOM}" #query_params
+    return f"&number={number_recipes}&offset={offset}" #query_params
 
 
 def Ingredients():
@@ -174,7 +242,7 @@ def Calories():
 
   
 def Category():
-    categories = {"Ingredientes": 1,"Calorias":2,"Receitas aleatórias":3}
+    categories = {"Ingredientes": 1,"Calorias":2,"Receitas aleatórias":3,"Tipo de refeição":4}
     print("Escolha uma categoria para pesquisar a receita: ")
     for category in categories:
         print(f"{categories[category]} : {category}")
@@ -193,13 +261,19 @@ def Category():
         except ValueError:
             print("Dado inválido, tente novamente")
             print("Escolha uma categoria para pesquisar a receita: ")
-            
-    if user_choice == 1:
-        query_params = Ingredients()
-    elif user_choice == 2:
-        query_params = Calories()
-    else:
-        query_params = Random()
+          
+
+    match user_choice:
+        case 1:
+            query_params = Ingredients()
+        case 2:
+            query_params = Calories()
+        case 3:
+            query_params = Random()
+        case 4:
+            query_params = Meal_type()
+    
+
 
 
     for category in categories:
@@ -216,8 +290,12 @@ def Query():
 
     if tag == "Ingredientes":
         query = f"https://api.spoonacular.com/recipes/findByIngredients?apiKey={API_KEY}" + query_params
-    else:
+
+    elif tag == "Calorias":
         query = f"https://api.spoonacular.com/recipes/findByNutrients?apiKey={API_KEY}" + query_params
+
+    else:
+        query = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}" + query_params
 
     response = requests.get(query) #Lista de receitas e suas informações
 
@@ -229,13 +307,16 @@ def Query():
         Query() 
 
 
-     
+    if tag== "Tipo de refeição" or tag == "Receitas aleatórias": #Pesquisas feita por ComplexSearch
+        for recipe in response.json()["results"]: 
+         recipes.append((recipe["title"],recipe["id"])) #Lista de tuplos com o nome e id da receita
 
-    for recipe in response.json():
-        if tag == "Calorias":
-            recipes.append((recipe["title"],recipe["id"],recipe["calories"])) #Lista de tuplos com o nome e id da receita
-        else:
-            recipes.append((recipe["title"],recipe["id"])) #Lista de tuplos com o nome e id da receita
+    else:
+        for recipe in response.json():
+            if tag == "Calorias":
+                recipes.append((recipe["title"],recipe["id"],recipe["calories"])) #Lista de tuplos com o nome e id da receita
+            else:
+                recipes.append((recipe["title"],recipe["id"])) #Lista de tuplos com o nome e id da receita
 
 
 
@@ -258,8 +339,15 @@ def main():
     Status()
     tag,recipes = Query()
     recipe = Ask_for_recipe(tag,recipes)
-    Recipe_instructions(recipe)
-    
+    image,meal_type,Ingredients = Get_Recipe_information(recipe[1]) #Informações sobre a receita selecionada através do id da receita
+    description,equipments = Get_and_Show_Recipe_instructions(recipe)
+
+        #Função que mostra as informações da receita selecionada
+        #É uma função de fase teste para ver as informações que vão ser armazenadas na base de dados
+    Show(image,meal_type,Ingredients,description,equipments)
+
+
+  
    
     
 if __name__ == "__main__":
