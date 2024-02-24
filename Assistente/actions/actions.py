7b4018f2022f9b3019f5246763bf326df3a5251e
David Palricas <davipalricas22@gmail.com>
2024-02-24 22:02:18 +0000

    Pesquisa por receitas aleatórias feitas pelo Assistente

--- Assistente/actions/actions.py
+++ Assistente/actions/actions.py
@@ -7,21 +7,134 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from .consts import API_KEYS, MAX_OFFSET # O "." é o caminho relativo para o arquivo consts.py
+                                        # É necessário meter este "."  caso contrário o rasa não consegue encontrar o arquivo consts.py
+import random
+import requests
+
+
+
+class ActionHelloWorld(Action):
+ 
+    def name(self) -> Text:
+        return "action_random_recipe"
+    
+    def Get_Recipe_instructions(self, recipe,api_key):
+        description = {} #Dicionário com o número do passo e a descrição do passo
+        all_equipaments = set() #Conjunto com todos os equipamentos necessários para a receita
+
+        recipe_id = recipe[1]
+        query = f"https://api.spoonacular.com/recipes/{recipe_id}/analyzedInstructions?apiKey={api_key}"
+        response_info = requests.get(query) #Informações da receita selecionada
+                                                    #Contém os passos , o número de cada passo, ingredientes e equipamentos necessários
+                  
+    
+        if response_info.json() == []: #Se a receita não tiver informação sobre os passos, ingredientes e equipamentos necessários
+         return None
+
+        for steps in response_info.json():
+            for step in steps["steps"]:
+                description[f"Passo nº {step['number']}"] = step["step"] #Dicionário com o número do passo e a descrição do passo
+
+          
+                for equipament in step["equipment"]:                   
+                    all_equipaments.add(equipament["name"]) #Adiciona o equipamento ao conjunto de  todos equipamentos  necessários para a receita
+                    
+        return description,all_equipaments
+    
+
+    def Get_Recipe_Info(self, recipe_id,api_key):
+        ingredients_info = {}
+        query = f"https://api.spoonacular.com/recipes/{recipe_id}/information?apiKey={api_key}"
+
+        response = requests.get(query) #Informações da receita selecionada
+
+        if response.json() == []: #Se a receita não tiver informação sobre os ingredientes necessários
+            return None
+    
+        image = response.json()["image"] # Url da imagem da receita
+
+        meal_type = response.json()["dishTypes"] #Tipo de refeição
+
+
+        for ingredient in response.json()["extendedIngredients"]:
+            ingredients_info[ingredient["name"]] = f"Quantidade {ingredient['measures']['metric']['amount']} {ingredient['measures']['metric']['unitShort']}"
+
+
+
+
+        return image,meal_type,ingredients_info
+
+
+    
+    def Query(self, params,api_key):
+         query = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + api_key + params
+         response = requests.get(query)
+         return response.json()
+    
+    def Random(self):
+        offset = random.randint(0, MAX_OFFSET)
+        number = 1 #Número de receitas que serão retornadas
+        return f"&offset={offset}&number={number}"
+    
+    def Status(self,api_key):
+        query_base = f"https://api.spoonacular.com/recipes/complexSearch?apiKey={api_key}"
+        response= requests.get(query_base)
+        status = response.status_code
+        if status== 200: #Código 200 indica que a conexão foi realizada com sucesso
+            print("\033[92mConexão realizada com sucesso\033[0m\n") #Código de cor verde para indicar que a conexão foi realizada com sucesso
+        elif status == 402: #Código 402 indica que a chave de API atingiu o limite de uso
+            i = API_KEYS.index(api_key)
+            
+            if i == len(API_KEYS)-1:
+                return None
+            else:
+                api_key = API_KEYS[i+1] #Muda a chave de API
+                self.Status(api_key) #Chama a função novamente para verificar se a nova chave de API está funcionando
+        else:
+            print("\033[91mErro na conexão\033[0m") #Código de cor vermelha para indicar que houve um erro na conexão
+            api_key = None
+            
+        return api_key
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+         
+         api_key = API_KEYS[0]
+         message = "Nehuma receita encontrada"
+         api_key = self.Status(api_key)
+
+         if api_key == None:
+             message = "Erro na conexão"
+             dispatcher.utter_message(text=message)
+             return []
+         
+
+         params = self.Random() #Parâmetros da receita aleatória
+
+         response = self.Query(params,api_key)
+
+         if response == []:
+            self.Random() #Se a API não retornar nenhuma receita, uma nova receita será selecionada
+         
+
+         for r in response["results"]:
+             recipe = (r["title"], r["id"]) 
+          
+                
+
+         if self.Get_Recipe_Info(recipe[1],api_key) == None or self.Get_Recipe_instructions(recipe,api_key) == None:
+               self.Random() #Se a receita não tiver informações suficientes, uma nova receita será selecionada
+         else:
+                image,meal_type,ingredients_info = self.Get_Recipe_Info(recipe[1],api_key)
+                description,all_equipaments = self.Get_Recipe_instructions(recipe,api_key)
+
+
+                message = f"\tReceita: {recipe[0]}\n\n\nImagem: {image}\n\n\nTipo de refeição: {meal_type}\n\n\nIngredientes: {ingredients_info}\n\n\nPassos: {description}\n\n\nEquipamentos: {all_equipaments}"
+
+         dispatcher.utter_message(text=message)
+         return []
---
+++ Assistente/actions/consts.py
@@ -0,0 +1,3 @@
+API_KEYS = ["ef6f2279b7864bad8ff9a04de2180657","34af4d2879884e459a8b2e5bb71d410e","d20575f54b404530829032207847afdb"] #Lista de chaves de API
+
+MAX_OFFSET = 900 #Número máximo de skips que a API permite
