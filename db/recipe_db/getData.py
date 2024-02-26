e0e805051a0db93ae8d28514da440cc22ff8df22
PedroMiguelTorresCarneiro <pedrocarneiro@ua.pt>
2024-02-26 18:49:23 +0000

    add methods to get cal from the API

--- db/recipe_db/getData.py
+++ db/recipe_db/getData.py
@@ -1,15 +1,20 @@
+import random
 import requests
 
 class GetData:
     def __init__(self):
-        # Chave do David
+        # Chave do 1
         #self.api_key = "ef6f2279b7864bad8ff9a04de2180657"
-        # Chave do Pedro
+        # Chave do 2
         #self.api_key = "34af4d2879884e459a8b2e5bb71d410e"
-        # Chave da Inês
+        # Chave da 3
         #self.api_key = "d20575f54b404530829032207847afdb"
-        # Chave do Ruben
-        #self.api_key = ""
+        # Chave do 4
+        self.api_key = "2971e205092f42268a2a1ab1e372124b"
+        # Chave do 5
+        #self.api_key = "ac793bb9af8340debe7e6145de267050"
+        # Chave do 6
+        #self.api_key = "af1671b748fb4583b803624de57fea7b"
 
         self.base_url = "https://api.spoonacular.com"
 
@@ -34,17 +39,33 @@ class GetData:
         data = response.json()
         return data.get('dishTypes', [])
 
+    def getRecipeNameByTag(self, tag="main course"):
+        # Retorna um nome de receita com base em uma tag (main course, dessert, appetizer)
+        query = f"{self.base_url}/recipes/complexSearch?apiKey={self.api_key}&query={tag}&number=1"
+        response = requests.get(query)
+        data = response.json()
+        if data['results']:
+            return data['results'][0]['title']
+        else:
+            return None
+        
     def getRecipe(self, tag="main course"):
         # Retorna um ID de receita com base em uma tag (main course, dessert, appetizer)
         query = f"{self.base_url}/recipes/complexSearch?apiKey={self.api_key}&query={tag}&number=1"
         response = requests.get(query)
         data = response.json()
-        #print(data)
+        print(data)
         if data['results']:
             return data['results'][0]['id']
         else:
             return None
 
+    # def getRandomRecipes(self, number="20"):
+    #     offset = random.ranint(0, 900)
+    #     query = f"{self.base_url}/recipes/random?apiKey={self.api_key}&number={number}&offset={offset}"
+    #     response = requests.get(query)
+    #     data = response.json()
+        
     def getRandomRecipes(self, number="20", tag="main course"):
         # Retorna uma lista de receitas com base no número e tag fornecidos, com uma tag padrão de "main course"
         query = f"{self.base_url}/recipes/complexSearch?apiKey={self.api_key}&number={number}&type={tag}"
@@ -155,36 +176,115 @@ class GetData:
             # Se ocorrer um erro na requisição, retornar uma mensagem de erro
             return f"Erro ao obter o cartão da receita: {response.status_code}"
 
+
+#
+# GET INGRIDIENT FACTS
+#
+    def search_ingredient_by_name(self, name):
+        """
+        Searches for an ingredient by name and returns its ID.
+
+        :param name: The name of the ingredient to search for.
+        :return: The ID of the first matching ingredient or None if not found.
+        """
+        search_url = f"{self.base_url}/food/ingredients/search?apiKey={self.api_key}&query={name}"
+        response = requests.get(search_url)
+        if response.status_code == 200:
+            results = response.json().get('results', [])
+            if results:
+                # Assuming you want the first result
+                return results[0]['id']
+        return None
+
+    def get_ingredient_info_by_name(self, name, amount=100, unit='grams'):
+        """
+        Gets detailed information about an ingredient by name.
+
+        :param name: The name of the ingredient.
+        :param amount: The amount of the ingredient (optional, default is 100).
+        :param unit: The unit of measure for the amount (optional, default is 'grams').
+        :return: A dictionary containing detailed information about the ingredient.
+        """
+        ingredient_id = self.search_ingredient_by_name(name)
+        if ingredient_id:
+            info_url = f"{self.base_url}/food/ingredients/{ingredient_id}/information?apiKey={self.api_key}&amount={amount}&unit={unit}"
+            response = requests.get(info_url)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return {"error": "Failed to fetch ingredient information"}
+        else:
+            return {"error": "Ingredient not found"}
     
+    def get_calories_by_ingredient_name(self, name, amount=100, unit='grams'):
+        """
+        Gets the calories value for an ingredient by name.
+
+        :param name: The name of the ingredient.
+        :return: The calories value of the ingredient or a message if not found.
+        """
+        ingredient_id = self.search_ingredient_by_name(name)
+        if ingredient_id:
+            info_url = f"{self.base_url}/food/ingredients/{ingredient_id}/information?apiKey={self.api_key}&amount=100&unit=grams"
+            response = requests.get(info_url)
+            if response.status_code == 200:
+                data = response.json()
+                nutrition = data.get('nutrition', {})
+                nutrients = nutrition.get('nutrients', [])
+                for nutrient in nutrients:
+                    if nutrient['name'] == "Calories":
+                        return {
+                            "Calories": f"{nutrient['amount']} {nutrient['unit']}"
+                        }
+                return {"error": "Calories information not found"}
+            else:
+                return {"error": "Failed to fetch ingredient information"}
+        else:
+            return {"error": "Ingredient not found"}
+        
 # Exemplo
+
+import translators as ts
+
 if __name__ == "__main__":
     data = GetData()
-    recipeId = data.getRecipe("main course")
-    print("Recipe ID:", recipeId)
-    recipeCardUrl = data.createRecipeCard("Baked Chicken", "Chicken, Salt, Pepper", "1. Preheat the oven to 350 degrees F. 2. Season the chicken with salt and pepper. 3. Bake for 30 minutes, or until the chicken is cooked through.", 30, 4, "https://spoonacular.com/recipeImages/123.jpg")
+    
+    
+    #recipeId = data.getRecipe("main course")
+    #print("Recipe ID:", recipeId)
+    #recipeCardUrl = data.createRecipeCard("Baked Chicken", "Chicken, Salt, Pepper", "1. Preheat the oven to 350 degrees F. 2. Season the chicken with salt and pepper. 3. Bake for 30 minutes, or until the chicken is cooked through.", 30, 4, "https://spoonacular.com/recipeImages/123.jpg")
     
     print("\n")
-    print("Random Recipes:", data.getRandomRecipes(10, "main course"))
+    pt_name = "maçã"
+    print("portuguese name:", pt_name)
+    en_name = ts.translate_text(pt_name, "bing", "pt", "en")
+    print("english name:", en_name)
+    print("Ingredient information:", data.get_calories_by_ingredient_name("apple"))
     print("\n")
-    print("Recipe ID:", recipeId)
-    print("\n")
-    print("Recipe Name:", data.getRecipeName(recipeId))
-    print("\n")
-    print("Image URL:", data.getRecipeImage(recipeId))
-    print("\n")
-    print("Ingredients:", data.getRecipeIngredients(recipeId))
-    print("\n")
-    print("Tools:", data.getRecipeTools(recipeId))
-    print("\n")
-    print("Steps:", data.getRecipeSteps(recipeId))
-    print("\n")
-    print("Categories:", data.getCategories(recipeId))
-    print("\n")
-    print("Servings:", data.getRecipeServings(recipeId))
-    print("\n")
-    print("Time:", data.getRecipeTime(recipeId))
-    print("\n")
-    print("Recipe Card:", data.getRecipeCard(recipeId))
-    print("\n")
-    print("Recipe Card:", recipeCardUrl)
-    print("\n")
+    # print("\n")
+    # print("Random Recipes:", data.getRandomRecipes(10, "main course"))
+    # print("\n")
+    # print("Recipe ID:", recipeId)
+    # print("\n")
+    # print("Recipe Name:", data.getRecipeName(recipeId))
+    # print("\n")
+    # print("Image URL:", data.getRecipeImage(recipeId))
+    # print("\n")
+    # print("Ingredients:", data.getRecipeIngredients(recipeId))
+    # print("\n")
+    # print("Tools:", data.getRecipeTools(recipeId))
+    # print("\n")
+    # print("Steps:", data.getRecipeSteps(recipeId))
+    # print("\n")
+    # print("Categories:", data.getCategories(recipeId))
+    # print("\n")
+    # print("Servings:", data.getRecipeServings(recipeId))
+    # print("\n")
+    # print("Time:", data.getRecipeTime(recipeId))
+    # print("\n")
+    # print("Recipe Card:", data.getRecipeCard(recipeId))
+    # print("\n")
+    # print("Recipe Name:", data.getRecipeNameByTag("dessert")) 
+    
+    #print("Recipe Card:", recipeCardUrl)
+    #print("\n")
