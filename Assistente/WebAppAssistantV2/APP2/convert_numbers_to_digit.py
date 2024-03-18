0356da75f7ef8efcfe3ed5719d021a10a740466e
PedroMiguelTorresCarneiro <pedrocarneiro@ua.pt>
2024-03-18 12:14:15 +0000

    minor fixes to this class

--- Assistente/WebAppAssistantV2/APP2/convert_numbers_to_digit.py
+++ Assistente/WebAppAssistantV2/APP2/convert_numbers_to_digit.py
@@ -45,6 +45,7 @@ def extract_and_convert_numeric_phrases(sentence, numbers_dict):
     
     return ' '.join(converted_words)
 
+# Dictionary with the numeric words and their respective values in Portuguese (feminine and masculine forms included)
 numbers_dict = {
     "zero": 0, "um": 1, "uma": 1, "dois": 2, "duas": 2, "três": 3, "tres": 3, "quatro": 4, "cinco": 5,
     "seis": 6, "sete": 7, "oito": 8, "nove": 9, "dez": 10, "onze": 11,
@@ -59,7 +60,6 @@ numbers_dict = {
     "mil": 1000, "milhão": 1000000, "milhões": 1000000,
     "milhares": 1000, "milhar": 1000
 }
-
 
 def main(argv):
     if len(argv) != 2:
