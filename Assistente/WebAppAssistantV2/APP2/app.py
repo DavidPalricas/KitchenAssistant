f082b2e5210bb5bf67789a8876f78b5dcd9494e4
PedroMiguelTorresCarneiro <pedrocarneiro@ua.pt>
2024-03-18 11:54:45 +0000

    add comments and a endpoint to convert numbers in digits

--- Assistente/WebAppAssistantV2/APP2/app.py
+++ Assistente/WebAppAssistantV2/APP2/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,request, jsonify
 import recipedb_queries as db
+import convert_numbers_to_digit as convert
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -33,7 +34,7 @@ CORS(app)
 #     app.run(debug=True) # Para mostrar os erros no browser
 
 
-
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH ALL RECIPES
 @app.route('/recipes', methods=['GET'])
 def fetch_recipes():
     """Fetch all recipes."""
@@ -49,12 +50,14 @@ def fetch_recipes():
     ]
     return jsonify(formatted_recipes)
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH RECIPE BY TAG
 @app.route('/recipe/tag/<tag>', methods=['GET'])
 def fetch_recipe_by_tag(tag):
     """Fetch recipes by tag."""
     recipe_ids = db.getRecipeByTag(tag)
     return jsonify({'recipe_ids': recipe_ids})
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH RECIPE BY NAME
 @app.route('/recipe/name/<name>', methods=['GET'])
 def fetch_recipe_by_name(name):
     """Fetch a recipe by name."""
@@ -64,18 +67,21 @@ def fetch_recipe_by_name(name):
     else:
         return jsonify({'error': 'Recipe not found'}), 404
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH INGREDIENTS FOR A GIVEN RECIPE ID
 @app.route('/recipe/<int:recipe_id>/ingredients', methods=['GET'])
 def fetch_ingredients(recipe_id):
     """Fetch ingredients for a given recipe ID."""
     ingredients = db.getIngredients(recipe_id)
     return jsonify(ingredients)
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH TOOLS FOR A GIVEN RECIPE ID
 @app.route('/recipe/<int:recipe_id>/tools', methods=['GET'])
 def fetch_tools(recipe_id):
     """Fetch tools for a given recipe ID."""
     tools = db.getTools(recipe_id)
     return jsonify(tools)
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH A RANDOM RECIPE
 @app.route('/recipe/random', methods=['GET'])
 def fetch_random_recipe():
     """Fetch a random recipe."""
@@ -86,24 +92,28 @@ def fetch_random_recipe():
     #print("recipe_name", recipe_name)
     return jsonify({'recipe_id': recipe_id, 'recipe_name': recipe_name, 'recipe_img': recipe_img})
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH NEXT INSTRUCTION FOR A GIVEN RECIPE ID AND CURRENT STEP
 @app.route('/recipe/<int:recipe_id>/next-instruction/<int:step>', methods=['GET'])
 def fetch_next_instruction(recipe_id, step):
     """Fetch the next instruction for a given recipe ID and current step."""
     next_instruction = db.getNextInstruction(recipe_id, step)
     return jsonify({'next_instruction': next_instruction})
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH PREVIOUS INSTRUCTION FOR A GIVEN RECIPE ID AND CURRENT STEP
 @app.route('/recipe/<int:recipe_id>/previous-instruction/<int:step>', methods=['GET'])
 def fetch_previous_instruction(recipe_id, step):
     """Fetch the previous instruction for a given recipe ID and current step."""
     previous_instruction = db.getPreviousInstruction(recipe_id, step)
     return jsonify({'previous_instruction': previous_instruction})
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH ACTUAL INSTRUCTION FOR A GIVEN RECIPE ID AND CURRENT STEP
 @app.route('/recipe/<int:recipe_id>/actual-instruction/<int:step>', methods=['GET'])
 def fetch_actual_instruction(recipe_id, step):
     """Fetch the previous instruction for a given recipe ID and current step."""
     actual_instruction = db.getActualInstruction(recipe_id, step)
     return jsonify({'actual_instruction': actual_instruction})
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH RECIPE NAME BY ID
 @app.route('/recipe/<int:recipe_id>/name', methods=['GET'])
 def fetch_recipe_name(recipe_id):
     """Fetch the name for a given recipe ID."""
@@ -113,6 +123,7 @@ def fetch_recipe_name(recipe_id):
     else:
         return jsonify({'error': 'Recipe not found'}), 404
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO FETCH RECIPE IMAGE
 @app.route('/recipe/<int:recipe_id>/image', methods=['GET'])
 def fetch_recipe_image(recipe_id):
     """Fetch the image URL for a given recipe ID."""
@@ -122,7 +133,23 @@ def fetch_recipe_image(recipe_id):
     else:
         return jsonify({'error': 'Image not found for the specified recipe'}), 404
 
+# ----------------------------------------------------------------------------------------- ENDPOINT TO CONVERT TEXT NUMBERS TO DIGITS    
+@app.route('/convert-text', methods=['POST'])
+def convert_text():
+    # Check if the request contains JSON data
+    if not request.json or 'text' not in request.json:
+        return jsonify({'error': 'Request must be JSON and contain a "text" field.'}), 400
+
+    text_to_convert = request.json['text']
+    
+    # Use the conversion function from your module
+    converted_text = convert.extract_and_convert_numeric_phrases(text_to_convert, convert.numbers_dict)
+    
+    if converted_text is not None:
+        return jsonify(converted_text)
+    else:
+        return jsonify({'error': 'Failed to convert text.'}), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-    
-    print("RANDOM RECIPE: ", db.getRandomRecipe())
