9683a9e8e887066d84d6a0186fa9202353a219ed
PedroMiguelTorresCarneiro <pedrocarneiro@ua.pt>
2024-04-18 20:18:27 +0100

    update email service

--- Assistente/WebAppAssistantV2/APP2/email_service.py
+++ Assistente/WebAppAssistantV2/APP2/email_service.py
@@ -22,9 +22,9 @@ def send_email(from_addr, to_addr, subject, body, smtp_server, smtp_port, passwo
     try:
         # Connect to the SMTP server and start TLS encryption
         with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.ehlo()  # Can be omitted
-            server.starttls()  # Secure the connection
-            server.ehlo()  # Can be omitted
+            server.ehlo()                                           # Can be omitted
+            server.starttls()                                       # Secure the connection
+            server.ehlo()                                           # Can be omitted
             server.login(from_addr, password)
             server.send_message(msg)
             return "Email sent successfully!"
@@ -32,17 +32,17 @@ def send_email(from_addr, to_addr, subject, body, smtp_server, smtp_port, passwo
         return f"Failed to send email: {e}"
 
 # # Example usage
-# smtp_server = 'smtp-mail.outlook.com'
-# smtp_port = 587
+#smtp_server = 'smtp-mail.outlook.com'
+#smtp_port = 587
 
-# # Email credentials
-# email = 'kitchen_assistant@outlook.com'
-# password = 'Start123.'  # Replace with the correct password
+# Email credentials
+#email = 'kitchen_assistant@outlook.com'
+#password = 'kitchen123.'  # Replace with the correct password
 
-# from_addr = email
-# to_addr = 'inesaguia@ua.pt'  # Change to the recipient's email
-# subject = 'Test Email from Python'
-# body = 'Hello, this is a test email sent from Python using SMTP with Outlook.'
+#from_addr = email
+#to_addr = 'inesaguia@ua.pt'  # Change to the recipient's email
+#subject = 'Test Email from Python'
+#body = 'Hello, this is a test email sent from Python using SMTP with Outlook.'
 
-# result = send_email(from_addr, to_addr, subject, body, smtp_server, smtp_port, password)
-# print(result)
+#result = send_email(from_addr, to_addr, subject, body, smtp_server, smtp_port, password)
+#print(result)
