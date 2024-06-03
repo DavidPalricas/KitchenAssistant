f4fb32b8df83fb21fd79ac576858a7cfc4b01b9a
davidpalricas <davidpalricas@ua.pt>
2024-06-03 19:40:46 +0100

    Falar pelo  chat bot

--- Assistente/WebAppAssistantV2/js/chat.js
+++ Assistente/WebAppAssistantV2/js/chat.js
@@ -9,30 +9,6 @@ $(document).ready(function() {
         $("#chat-box").add("d-none");
     });
 
-    $("#chat-input").keypress(function(event) {
-        if (event.which === 13) { //Se a tecla pressionada for enter
-            let message = $(this).val(); //Extrai o valor do campo de texto
 
-
-            $(this).val(""); //Limpa o campo de texto
-
-            $("#chat-messages").append("<div><strong>Você:</strong>" + message + "</div>"); //Adiciona a mensagem no chat
-
-            $.ajax({
-                type: "POST",
-                url: "/webhook",
-                contentType: "application/json",
-                data: JSON.stringify({ message: message }),
-                success: function(data) {
-                    let rasa_response = data.response;
-                    $("#chat-messages").append("<div><strong>Assistente:</strong>" + rasa_response + "</div>");
-                }
-
-
-
-            });
-
-        }
-    });
 
 })
